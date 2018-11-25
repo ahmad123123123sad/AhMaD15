3e3512f861930caef638d9bcd199e00db88288e6
ahmad123123123sad <45146326+ahmad123123123sad@users.noreply.github.com>
2018-11-25 21:19:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,8 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
  
 client.on('ready', async() => {
-var server = "513743320703434752"; 
-var channel = "513743478325510144";
+var server = "496762158000898049"; 
+var channel = "516330973172989963";
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('** احمد عمك يا ورعاحمد عمك يا ورع احمد عمك يا ورع احمد عمك يا ورع احمد عمك يا ورع احمد عمك يا ورع احمد عمك يا ورع احمد عمك يا ورع **')
     },305);
